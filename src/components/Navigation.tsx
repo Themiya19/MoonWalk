d606e79e48b2959e4ef3925277f970416ef99731
Themiya Wanigasekara <themiyawanigasekara19@gmail.com>
2024-12-03 12:26:23 +0530

    second comit

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -144,7 +144,9 @@ const Navigation = () => {
               <a
                 key={link.href}
                 href={link.href}
-                className={`block px-3 py-2 text-base ${getLinkStyles(link.href)}`}
+                className={`block px-3 py-2 text-base text-white hover:text-indigo-400 ${
+                  link.href.slice(1) === activeSection ? 'text-indigo-400 font-medium' : ''
+                }`}
                 onClick={() => setIsMobileMenuOpen(false)}
               >
                 {link.label}
